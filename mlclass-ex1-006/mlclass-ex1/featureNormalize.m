95674b8d9a43311e7448b7da0912c7a5b9a7008d
tangbin90 <tangbin90@gmail.com>
2014-07-22 01:15:02 +0800

    new file:   mlclass-ex1-006/ex1.pdf 	new file:   mlclass-ex1-006/mlclass-ex1/computeCost.asv 	new file:   mlclass-ex1-006/mlclass-ex1/computeCost.m 	new file:   mlclass-ex1-006/mlclass-ex1/computeCostMulti.m 	new file:   mlclass-ex1-006/mlclass-ex1/ex1.asv 	new file:   mlclass-ex1-006/mlclass-ex1/ex1.m 	new file:   mlclass-ex1-006/mlclass-ex1/ex1_multi.asv 	new file:   mlclass-ex1-006/mlclass-ex1/ex1_multi.m 	new file:   mlclass-ex1-006/mlclass-ex1/ex1data1.txt 	new file:   mlclass-ex1-006/mlclass-ex1/ex1data2.txt 	new file:   mlclass-ex1-006/mlclass-ex1/featureNormalize.asv 	new file:   mlclass-ex1-006/mlclass-ex1/featureNormalize.m 	new file:   mlclass-ex1-006/mlclass-ex1/gradientDescent.asv 	new file:   mlclass-ex1-006/mlclass-ex1/gradientDescent.m 	new file:   mlclass-ex1-006/mlclass-ex1/gradientDescentMulti.m 	new file:   mlclass-ex1-006/mlclass-ex1/matlab.mat 	new file:   mlclass-ex1-006/mlclass-ex1/ml_login_data.mat 	new file:   mlclass-ex1-006/mlclass-ex1/normalEqn.asv 	new file:   mlclass-ex1-006/mlclass-ex1/normalEqn.m 	new file:   mlclass-ex1-006/mlclass-ex1/plotData.m 	new file:   mlclass-ex1-006/mlclass-ex1/submit.m 	new file:   mlclass-ex1-006/mlclass-ex1/submitWeb.m 	new file:   mlclass-ex1-006/mlclass-ex1/warmUpExercise.m 	new file:   mlclass-ex2-006/ex2.pdf 	new file:   mlclass-ex2-006/mlclass-ex2/costFunctionReg.asv 	new file:   mlclass-ex2-006/mlclass-ex2/ex2.m 	new file:   mlclass-ex2-006/mlclass-ex2/ex2_reg.m 	new file:   mlclass-ex2-006/mlclass-ex2/ex2data1.txt 	new file:   mlclass-ex2-006/mlclass-ex2/ex2data2.txt 	new file:   mlclass-ex2-006/mlclass-ex2/mapFeature.m

---
+++ mlclass-ex1-006/mlclass-ex1/featureNormalize.m
@@ -0,0 +1,45 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+% You need to set these values correctly
+X_norm = X;
+mu = zeros(1, size(X_norm, 2));
+sigma = zeros(1, size(X_norm, 2));
+mu=sum(X_norm)/size(X_norm,1);
+for iter=1:size(X_norm,2),
+    X_norm(:,iter)=X_norm(:,iter)-mu(iter);
+    sigma(iter)=std(X_norm(:,iter));
+    X_norm(:,iter)=X_norm(:,iter)/sigma(iter);
+end
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: First, for each feature dimension, compute the mean
+%               of the feature and subtract it from the dataset,
+%               storing the mean value in mu. Next, compute the 
+%               standard deviation of each feature and divide
+%               each feature by it's standard deviation, storing
+%               the standard deviation in sigma. 
+%
+%               Note that X is a matrix where each column is a 
+%               feature and each row is an example. You need 
+%               to perform the normalization separately for 
+%               each feature. 
+%
+% Hint: You might find the 'mean' and 'std' functions useful.
+%       
+
+
+
+
+
+
+
+
+
+% ============================================================
+
+end
